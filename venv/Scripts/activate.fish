573f80ad9b35972d9fc5255a7a2d467ba896cfcf
Efren <l19590341@sjuanrio.tecnm.mx>
2025-04-07 18:11:43 -0600

    Crea entorno virtual y agrega dependencias

--- venv/Scripts/activate.fish
+++ venv/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\Users\efren\OneDrive\Documentos\tiendaderopa\venv'
+set -gx VIRTUAL_ENV 'C:\Users\efren\OneDrive\Documentos\GitHub\tiendaderopa\venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
